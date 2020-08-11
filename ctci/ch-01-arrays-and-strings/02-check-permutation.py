482add1e1be6e33f6838bf66a5294a3a531869de
AlveeM <alvee.akand@outlook.com>
2020-08-11 09:50:25 -0400

    added 02-check-permutation to ctci ch 01

---
+++ ctci/ch-01-arrays-and-strings/02-check-permutation.py
@@ -0,0 +1,55 @@
+import unittest
+
+# TC: O(n log n)
+def check_permutation(str1, str2):
+    str1_len = len(str1)
+    str2_len = len(str2)
+
+    if str1_len != str2_len: return False 
+
+    str1_sorted = ''.join(sorted(str1))
+    str2_sorted = ''.join(sorted(str2))
+
+    return str1_sorted == str2_sorted
+
+# TC: O(n)
+# def check_permutation(str1, str2):
+#     if len(str1) != len(str2): return False
+
+#     counter = {}
+
+#     for char in str1:
+#         counter[char] = counter.get(char, 0) + 1
+#     print(counter)
+#     for char in str2:
+#         if char not in counter:
+#             return False
+#         counter[char] -= 1
+
+#     return True
+
+class Test(unittest.TestCase):
+    dataT = (
+        ('abcd', 'bacd'),
+        ('3563476', '7334566'),
+        ('wef34f', 'wffe34'),
+    )
+    dataF = (
+        ('abcd', 'd2cba'),
+        ('2354', '1234'),
+        ('dcw4f', 'dcw5f'),
+    )
+
+    def test_cp(self):
+        # true check
+        for test_strings in self.dataT:
+            result = check_permutation(*test_strings)
+            self.assertTrue(result)
+        # false check
+        for test_strings in self.dataF:
+            result = check_permutation(*test_strings)
+            self.assertFalse(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
