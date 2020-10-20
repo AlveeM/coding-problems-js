5df0aca511f1c266805d556f7ac6cb146147c7e1
AlveeM <alvee.akand@outlook.com>
2020-10-20 10:51:44 -0400

    added Pascal's Triangle II

--- leetcode/recursion-1/05_pascals_triangle_II.py
+++ leetcode/recursion-1/05_pascals_triangle_II.py
@@ -1,5 +1,31 @@
 from typing import List
 
+# Iterative
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        pass
+        result = [1] * (rowIndex + 1)
+        for i in range(1, rowIndex):
+            for j in range(i, 0, -1):
+                result[j] = result[j] + result[j-1]
+        
+        return result
+
+# Recursive
+class Solution2:
+    def __init__(self):
+        self.cache = {}
+        
+    def getRow(self, rowIndex: int) -> List[int]:
+        result = []
+        for colIndex in range(rowIndex + 1):
+            result.append(self.getCellVal(rowIndex, colIndex))
+        return result
+    
+    def getCellVal(self, rowIdx, colIdx):
+        if colIdx == 0 or colIdx == rowIdx: return 1
+        
+        self.cache[(rowIdx-1, colIdx-1)] = self.cache.get((rowIdx-1, colIdx-1)) or self.getCellVal(rowIdx-1, colIdx-1)
+        
+        self.cache[(rowIdx-1, colIdx)] = self.cache.get((rowIdx-1, colIdx)) or self.getCellVal(rowIdx-1, colIdx)
+        
+        return self.cache[(rowIdx-1, colIdx-1)] + self.cache[(rowIdx-1, colIdx)]
