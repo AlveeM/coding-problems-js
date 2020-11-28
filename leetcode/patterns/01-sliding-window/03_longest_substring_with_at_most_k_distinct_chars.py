3ef7a20f51759c50c870e4cb0a95ab10370db295
AlveeM <alvee.akand@outlook.com>
2020-11-27 23:57:05 -0500

    modified function signature

--- leetcode/patterns/01-sliding-window/03_longest_substring_with_at_most_k_distinct_chars.py
+++ leetcode/patterns/01-sliding-window/03_longest_substring_with_at_most_k_distinct_chars.py
@@ -1,27 +1,28 @@
 # https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
 
-def lengthOfLongestSubstringKDistinct(s, k):
-  max_len = 0
-  char_freq = {}
-  window_start = 0
+class Solution:
+  def lengthOfLongestSubstringKDistinct(self, s, k):
+    max_len = 0
+    char_freq = {}
+    window_start = 0
 
-  for window_end in range(len(s)):
-    end_char = s[window_end]
+    for window_end in range(len(s)):
+      end_char = s[window_end]
 
-    if end_char not in char_freq:
-      char_freq[end_char] = 0
-    char_freq[end_char] += 1
+      if end_char not in char_freq:
+        char_freq[end_char] = 0
+      char_freq[end_char] += 1
 
-    while len(char_freq) > k:
-      start_char = s[window_start]
-      char_freq[start_char] -= 1
-      if char_freq[start_char] == 0:
-        del char_freq[start_char]
-      window_start += 1
-    
-    max_len = max(max_len, window_end - window_start + 1)
+      while len(char_freq) > k:
+        start_char = s[window_start]
+        char_freq[start_char] -= 1
+        if char_freq[start_char] == 0:
+          del char_freq[start_char]
+        window_start += 1
+      
+      max_len = max(max_len, window_end - window_start + 1)
 
-  return max_len
+    return max_len
 
 # s = "eceba"
 # k = 2
