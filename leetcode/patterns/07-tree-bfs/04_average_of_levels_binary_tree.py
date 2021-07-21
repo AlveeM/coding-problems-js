020bffbd14ca9993f1e678181ee7df761f1533de
Alvee Akand <alvee.akand@outlook.com>
2021-07-21 11:16:10 -0400

    updated code for average_of_levels_binary_tree

--- leetcode/patterns/07-tree-bfs/04_average_of_levels_binary_tree.py
+++ leetcode/patterns/07-tree-bfs/04_average_of_levels_binary_tree.py
@@ -21,3 +21,28 @@ class Solution:
             res.append(total / count)
             
         return res
+
+class Solution2:
+    def averageOfLevels(self, root):
+        levels = []
+        self.helper(root, levels, 0)
+
+        # res = [level_obj["sum"] / level_obj["count"] for level_obj in levels]
+        res = []
+        for level_obj in levels:
+            avg = level_obj["sum"] / level_obj["count"]    
+            res.append(avg)
+
+        return res
+        
+    def helper(self, node, levels, level):
+        if node is None: return
+        
+        if level == len(levels):
+            levels.append({ "sum": node.val, "count": 1 })
+        else:
+            levels[level]["sum"] += node.val
+            levels[level]["count"] += 1
+            
+        self.helper(node.left, levels, level + 1)
+        self.helper(node.right, levels, level + 1)
